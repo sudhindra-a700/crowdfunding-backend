c45112911fea125705bc969ca07c207147429c7d
sudhindra-a700 <prakashsudhindra@gmail.com>
2025-08-05 18:42:01 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Use the PORT environment variable provided by Render, or default to 8000
-UVICORN_PORT=${PORT:-10000}
-
-# Start uvicorn
-
-exec uvicorn app:app --host 0.0.0.0 --port $UVICORN_PORT --workers 1 --timeout-keep-alive 30
